9a6852a95e9cc93aba20d32197c7e04d061e493d
Bruknown <brunobrandini77@gmail.com>
2020-09-02 19:52:57 -0300

    v0.1
    /Bruno

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main
     {
         String nome = args.length > 0 ? args[0] : "world";
         System.out.println(nome + "!");
-        
+        if (nome.equals("Bruno") == true)
+        {
+            System.out.println("Matricula: 192072006");
+        }
     }
 }
