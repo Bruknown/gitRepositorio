12c4849383ca7968d6aab2b8b1f0d7042428fb74
Bruknown <brunobrandini77@gmail.com>
2020-09-02 19:36:48 -0300

    Prova
    /Bruno

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main 
+{
+    public static void main(String... args)
+    {
+        String nome = args.length > 0 ? args[0] : "world";
+        System.out.println(nome + "!");
+    }
+}
